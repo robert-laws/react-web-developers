5686a38a43818c0d99442c20b539cea38d5b278a
Robert Laws <roblaws@me.com>
2019-07-29 16:49:43 -0400

    reset application

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
-import Movie from './components/Movie';
-import MovieForm from './components/MovieForm';
 
 class App extends Component {
-  state = {
-    title: '',
-    director: '',
-    click: false
-  }
-
-  handleMovieSubmit = (title, director) => {
-    this.setState({
-      title,
-      director
-    })
-  }
-
-  updateClick = () => {
-    this.setState({
-      click: !this.state.click
-    })
-  }
+  
 
   render() {
     return (
       <div>
-        <h2>App...</h2>
-        <MovieForm onMovieSubmit={this.handleMovieSubmit} />
-        <Movie title={this.state.title} director={this.state.director} />
-        <div>
-          Click: {this.state.click.toString()}
-        </div>
-        <button onClick={this.updateClick}>Update Click</button>
+        App...
       </div>
     )
   }
